3888eceeb0cba0c7c49dca84656211b0062ffb3f
Kiwifuit <mahkiwi123@gmail.com>
2022-09-02 09:54:40 +0800

    Added tests for math ops

--- src/internal/op.rs
+++ src/internal/op.rs
@@ -27,3 +27,49 @@ pub fn exponent(left: f64, right: f64) -> Option<f64> {
 
     Some(res)
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn add_works() {
+        let result = add(9.0, 10.0);
+
+        assert_eq!(result, Some(19.0));
+    }
+
+    #[test]
+    fn subtract_works() {
+        let positive = subtract(19.0, 3.0);
+        let negative = subtract(3.0, 4.0);
+
+        assert_eq!(positive, Some(16.0));
+        assert_eq!(negative, Some(-1.0));
+    }
+
+    #[test]
+    fn multiply_works() {
+        let twelve = multiply(3.0, 4.0);
+        let zero = multiply(39.0, 0.0);
+
+        assert_eq!(twelve, Some(12.0));
+        assert_eq!(zero, Some(0.0));
+    }
+
+    #[test]
+    fn divide_works() {
+        let none = divide(3.0, 4.0);
+        let five = divide(26.0, 5.0);
+
+        assert_eq!(none, None);
+        assert_eq!(five, Some(5.2));
+    }
+
+    #[test]
+    fn exponent_works() {
+        let result = exponent(3.0, 4.0);
+
+        assert_eq!(result, Some(81));
+    }
+}
