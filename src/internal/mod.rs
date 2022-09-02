f2e85189699116baf060917f7250346d46d83dbb
Kiwifuit <mahkiwi123@gmail.com>
2022-09-02 10:34:34 +0800

    Added tests for MathOperator and MathOperation

--- src/internal/mod.rs
+++ src/internal/mod.rs
@@ -3,6 +3,7 @@ use std::str::FromStr;
 mod op;
 mod parser;
 
+#[derive(Debug, PartialEq, Eq)]
 pub enum MathOperators {
     Add,
     Subtract,
@@ -45,11 +46,76 @@ pub struct MathOperation {
 }
 
 impl MathOperation {
+    fn new(left: f64, right: f64, opcode: MathOperators) -> Self {
+        Self {
+            left,
+            right,
+            opcode,
+        }
+    }
+
     pub fn perform(&self) -> f64 {
         let operation = self.opcode.to_callback();
         match operation(self.left, self.right) {
             Some(num) => num,
             None => 0.0,
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::{MathOperation, MathOperators};
+
+    #[test]
+    fn math_op_translation() {
+        let a = "+".parse::<MathOperators>();
+        let s = "-".parse::<MathOperators>();
+        let m = "*".parse::<MathOperators>();
+        let d = "/".parse::<MathOperators>();
+        let e = "^".parse::<MathOperators>();
+
+        let f = "?".parse::<MathOperators>();
+
+        assert_eq!(a.unwrap(), MathOperators::Add);
+        assert_eq!(s.unwrap(), MathOperators::Subtract);
+        assert_eq!(m.unwrap(), MathOperators::Multiply);
+        assert_eq!(d.unwrap(), MathOperators::Divide);
+        assert_eq!(e.unwrap(), MathOperators::Exponent);
+        assert!(f.is_err());
+    }
+
+    #[test]
+    fn math_op_perform() {
+        let add = MathOperation::new(1.0, 2.0, MathOperators::Add);
+        let sub = MathOperation::new(3.0, 2.0, MathOperators::Subtract);
+        let mul = MathOperation::new(2.0, 2.0, MathOperators::Multiply);
+        let div = MathOperation::new(10.0, 5.0, MathOperators::Divide);
+        let exp = MathOperation::new(2.0, 2.0, MathOperators::Exponent);
+
+        assert_eq!(add.left, 1.0);
+        assert_eq!(add.right, 2.0);
+        assert_eq!(add.opcode, MathOperators::Add);
+        assert_eq!(add.perform(), 3.0);
+
+        assert_eq!(sub.left, 3.0);
+        assert_eq!(sub.right, 2.0);
+        assert_eq!(sub.opcode, MathOperators::Subtract);
+        assert_eq!(sub.perform(), 1.0);
+
+        assert_eq!(mul.left, 2.0);
+        assert_eq!(mul.right, 2.0);
+        assert_eq!(mul.opcode, MathOperators::Multiply);
+        assert_eq!(mul.perform(), 4.0);
+
+        assert_eq!(div.left, 10.0);
+        assert_eq!(div.right, 5.0);
+        assert_eq!(div.opcode, MathOperators::Divide);
+        assert_eq!(div.perform(), 2.0);
+
+        assert_eq!(exp.left, 2.0);
+        assert_eq!(exp.right, 2.0);
+        assert_eq!(exp.opcode, MathOperators::Exponent);
+        assert_eq!(exp.perform(), 4.0);
     }
 }
