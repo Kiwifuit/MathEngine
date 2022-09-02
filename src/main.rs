faead793b9c36620e5acc566994fd57e908c7b2f
Kiwifuit <mahkiwi123@gmail.com>
2022-09-02 11:05:34 +0800

    Added prompt function

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,34 @@
+use std::io::{stdin, stdout, Write};
+use std::str::FromStr;
+
 mod internal;
+
+pub fn prompt<R>(message: &str, newline: bool) -> Option<R>
+where
+    R: FromStr,
+    <R as FromStr>::Err: std::fmt::Display,
+{
+    loop {
+        let mut raw = String::new();
+
+        if newline {
+            println!("{}", message);
+        } else {
+            print!("{}", message);
+            stdout().flush().unwrap();
+        }
+
+        stdin()
+            .read_line(&mut raw)
+            .ok()
+            .expect("Unble to get user input");
+
+        match raw.trim().parse::<R>() {
+            Ok(parsed) => return Some(parsed),
+            Err(err) => println!("An Error Occured: {}", err),
+        };
+    }
+}
 
 fn main() {
     println!("Hello, world!");
